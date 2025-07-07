ad4d343c8b15729a24ab69860e25f140092ba926
sivajithms <sivajithm@gmail.com>
2025-07-07 15:21:28 +0530

    solved fox and snake

---
+++ implementation/Fox And Snake/foxAndSnake.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+
+    for(int i = 1; i <= n; i++){
+        if(i & 1) cout<< string(m, '#');
+        else {
+            if((i / 2) & 1) cout<< string(m - 1, '.') << '#';
+            else cout << '#' << string(m - 1, '.');
+        }
+        cout<<endl;
+    }
+    return 0;
+}
